5f09eea3a2bd284542c5cd97485f229c9e12b061
Tom Franklin <github@thomasfranklin.co.uk>
2019-10-18 00:29:08 +0100

    Added location file to store references in a class object.

---
+++ Location.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Location
+{
+    private UInt16 xLocation;
+    private UInt16 yLocation;
+    private UInt16 hashLocation;
+
+    public Location(UInt16 x, UInt16 y, UInt16 hashLoc)
+    {
+        xLocation = x;
+        yLocation = y;
+        hashLocation = hashLoc;
+    }
+
+    public UInt16 getX()
+    {
+        return xLocation;
+    }
+
+    public UInt16 getY()
+    {
+        return yLocation;
+    }
+
+    public UInt16 getHashLocation()
+    {
+        return hashLocation;
+    }
+
+    public void setX(UInt16 x)
+    {
+        xLocation = x;
+    }
+
+    public void setY(UInt16 y)
+    {
+        yLocation = y;
+    }
+
+    public void setHashLocation(UInt16 hash)
+    {
+        hashLocation = hash;
+    }
+}
