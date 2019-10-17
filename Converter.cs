9b01b83ca3572ba5c19bd93f2308be9f7a561354
Tom Franklin <github@thomasfranklin.co.uk>
2019-10-18 00:29:40 +0100

    Added converter file to deal with Ascii to Hex and vice versa.

---
+++ Converter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+public class Converter
+{   
+    public static string asciiToHex(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+        foreach (byte b in inputBytes)
+        {
+            sb.Append(string.Format("{0:x2}", b));
+        }
+
+        return sb.ToString();
+    }
+
+    public static string hexToAscii(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < input.Length; i = i + 2)
+        {
+            string charConvert = input.Substring(i, 2);
+
+            int lNumber = Convert.ToInt32(charConvert, 16);
+            char c = (char) lNumber;
+
+            sb.Append(c.ToString());
+        }
+
+        return sb.ToString();
+    }
+}
