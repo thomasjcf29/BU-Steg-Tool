ac498169f926391e463279bafa3977086d87d9a5
Tom Franklin <github@thomasfranklin.co.uk>
2019-10-25 16:02:35 +0100

    Comments completed for this class.

--- Program.cs
+++ Program.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 
 namespace FrankStore
 {
+    /// <summary>
+    /// Class <c>Program</c> create the object to manage the program.
+    /// </summary>
+    /// <para>
+    /// This class is what will file off the Encoding/Decoding class and hand all permissions down to those classes.
+    /// </para>
     public class Program
     {
         private bool encoding;
 
+        /// <summary>
+        /// Static entry method to this program, will instantly create the <c>Program</c> class.
+        /// </summary>
+        /// <param name="args">
+        /// Arguments to be passed into the program.
+        /// </param>
         // ReSharper disable once InconsistentNaming - Main method has to be capitals
         public static void Main(string[] args)
         {
-            // ReSharper disable once ObjectCreationAsStatement - Don't need to keep this assignment once it's done the program can close.
+            // ReSharper disable once ObjectCreationAsStatement - Don't need to keep this assignment once it's done the
+            // program can close.
             new Program(args);
         }
 
+        /// <summary>
+        /// Class initiator this method creates a new <c>Program</c> class.
+        /// </summary>
+        /// <param name="args">
+        /// Arguments to be passed into the program.
+        /// </param>
+        /// <returns>
+        /// The class <c>Program</c>.
+        /// </returns>
         private Program(IReadOnlyList<string> args)
         {
             Console.WriteLine("Welcome To FrankStore!");
@@ -28,6 +50,16 @@ namespace FrankStore
             Console.WriteLine("Operation completed, closing.");
         }
 
+        /// <summary>
+        /// Run tasks will take the parameters and either encode or decode the file.
+        /// </summary>
+        /// <para>
+        /// If encoding it will create the <c>FrankEncoding</c> class, if decoding it will create the <c>FrankDecoding</c>
+        /// class.
+        /// </para>
+        /// <param name="coverFile">The image to use which hides the data.</param>
+        /// <param name="inputFile">The file you wish to encode/decode.</param>
+        /// <param name="outputFile">The file you wish to output it too.</param>
         private void runTasks(string coverFile, string inputFile, string outputFile)
         {
             if (encoding)
@@ -54,6 +86,11 @@ namespace FrankStore
             }
         }
 
+        /// <summary>
+        /// Checks the input arguments to ensure all the required values are provided.
+        /// </summary>
+        /// <param name="args">The arguments provided to the program</param>
+        /// <returns>Returns <c>true</c> if the arguments are valid or <c>false</c> if they are not.</returns>
         private bool checkValidation(IReadOnlyList<string> args)
         {
             //Check to make sure all arguments are provided
@@ -81,6 +118,9 @@ namespace FrankStore
             return true;
         }
 
+        /// <summary>
+        /// Displays a message to console saying what the valid arguments are with examples.
+        /// </summary>
         private static void displayArguments()
         {
             Console.Error.WriteLine("[ERROR]: Please provide arguments to this program.");
