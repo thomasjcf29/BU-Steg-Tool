07123a15d0e93bf3b33acbc453c23d9e7b5da00e
Tom Franklin <github@thomasfranklin.co.uk>
2019-10-25 17:14:51 +0100

    Comments completed for this class.

--- Converter.cs
+++ Converter.cs
@@ -1,11 +1,23 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace FrankStore
 {
+    /// <summary>
+    /// Class <c>Converter</c> is in charge of converting from specified inputs to outputs.
+    /// </summary>
+    /// <para>
+    /// For example, this class can convert byte to hex and back as well as hex to int.
+    /// </para>
     public static class Converter
     {
-        public static string byteToHex(byte[] input)
+        /// <summary>
+        /// Returns back the hex representation of the provided bytes.
+        /// </summary>
+        /// <param name="input">Bytes you wish to convert to their hex representation.</param>
+        /// <returns>Hex representation of the provided bytes.</returns>
+        public static string byteToHex(IEnumerable<byte> input)
         {
             var sb = new StringBuilder();
 
@@ -17,6 +29,11 @@ namespace FrankStore
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Returns back the byte[] representation of the provided hex.
+        /// </summary>
+        /// <param name="input">Hex you wish to convert to their bytes representation.</param>
+        /// <returns>Byte representation of the hex you provided.</returns>
         public static byte[] hexToByte(string input)
         {
             var charNumber = input.Length;
@@ -29,11 +46,27 @@ namespace FrankStore
             return bytes;
         }
 
+        /// <summary>
+        /// Returns back the hex representation of the int you provided.
+        /// </summary>
+        /// <para>
+        /// I.E. 0 = 0, 16 = F.
+        /// </para>
+        /// <param name="input">Int you would like to convert to hex.</param>
+        /// <returns>Hex representation of the int you provided.</returns>
         public static string intToHex(int input)
         {
             return input.ToString("X");
         }
 
+        /// <summary>
+        /// Returns back the int representation of the hex you provided.
+        /// </summary>
+        /// <para>
+        /// I.E. F = 16, 0 = 0.
+        /// </para>
+        /// <param name="input">Hex you would like to convert to an int.</param>
+        /// <returns>Int representation of the hex you provided.</returns>
         public static int hexToInt(string input)
         {
             return int.Parse(input, System.Globalization.NumberStyles.HexNumber);
